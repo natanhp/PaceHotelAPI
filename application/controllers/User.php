49256e81644ad976a1dd5f860ecd9a733408b171
Natan Hari Pamungkas <natanhp@protonmail.com>
2019-11-21 21:20:23 +0700

    Finish user CRUD
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

---
+++ application/controllers/User.php
@@ -0,0 +1,122 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+use chriskacerguis\RestServer\RestController;
+
+class User extends RestController{
+    public function __construct(){
+        // header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: GET, OPTIONS, POST, DELETE');
+        header('Access-Control-Allow-Headers: Content-Type, Content-Length, Accept-Encoding, Authorization');
+        parent::__construct();
+        $this->load->model('UserModel');
+        $this->load->library(['form_validation', 'email']);
+        $this->load->helper(['jwt', 'authorization', 'email']);
+    }
+
+    public function index_get(){
+        return $this->returnData($this->db->get('Customer')->result(), false);
+    }
+
+    public function login_post(){
+        $user = new UserData();
+        $user->password = $this->post('password');
+        $user->email = $this->post('email');
+
+        $response = $this->UserModel->login($user);
+       
+
+        return $this->response(['message' => $response['msg'], 'error' => $response['error'], 'token' => $response['token']]);
+    }
+
+    public function index_post($id = null){
+        $validation = $this->form_validation;
+        $rule = $this->UserModel->rules();
+
+        if($id == null){
+            array_push($rule,
+            [
+                'field' => 'password',
+                'label' => 'password',
+                'rules' => 'required'
+            ],
+            [
+                'field' => 'email',
+                'label' => 'email',
+                'rules' => 'required|valid_email|is_unique[Customer.email]'
+            ]);
+        }else{
+            array_push($rule,
+            [
+                'field' => 'email',
+                'label' => 'email',
+                'rules' => 'required|valid_email'
+            ]);
+        }
+
+        $validation->set_rules($rule);
+        if(!$validation->run()){
+            return $this->returnData($this->form_validation->error_array(), true);
+        }
+
+        $user = new UserData();
+        $user->name = $this->post('name');
+        $user->password = $this->post('password');
+        $user->email = $this->post('email');
+
+        if($id == null){
+            $token = uniqid(true);
+            $user->token = $token;
+            $this->sendMail($user->email, $token);
+            $response = $this->UserModel->store($user);
+        }else{
+            $response = $this->UserModel->update($user, $id);
+        }
+
+        return $this->returnData($response['msg'], $response['error']);
+    }
+
+    public function emailverification_get($token){
+        $user = new UserData();
+        $user->verified = "1";
+        
+        $response = $this->UserModel->emailVerification($user, $token);
+
+        return $this->returnData($response['msg'], $response['error']);
+    }
+
+    public function returnData($msg, $error){
+        $response['error'] = $error;
+        $response['message'] = $msg;
+
+        return $this->response($response);
+    }
+
+    private function sendMail($recipient, $token){
+        $ci = get_instance();
+        $config['protocol'] = "smtp";
+        $config['smtp_host'] = "mail.natanhp.id";
+        $config['smtp_port'] = "465";
+        $config['smtp_crypto'] = 'ssl';
+        $config['smtp_user'] = "pacehotel@natanhp.id";
+        $config['smtp_pass'] = "2eO=UCXH)M?d";
+        $config['charset'] = "utf-8";
+        $config['mailtype'] = "html";
+        $config['newline'] = "\r\n";
+        $ci->email->initialize($config);
+        $ci->email->from('pacehotel@natanhp.id', 'Pace-Hotel');
+        $ci->email->to($recipient);
+        $ci->email->subject('Email Verification');
+        $ci->email->message('Please follow this link: http://localhost/~ned/backend/index.php/user/emailverification/'.$token);
+        $this->email->send();
+    }
+}
+
+class UserData{
+    public $name;
+    public $password;
+    public $email;
+    public $token;
+    public $verified;
+}
+?>
---
+++ application/models/UserModel.php
@@ -0,0 +1,72 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class UserModel extends CI_Model{
+    private $table = 'Customer';
+
+    public $id;
+    public $name;
+    public $email;
+    public $password;
+    public $token;
+    public $verified;
+    
+    public function rules(){
+        return [
+                [
+                    'field' => 'name',
+                    'label' => 'name',
+                    'rules' => 'required'
+                ]
+            ];
+    }
+
+    public function getAll(){
+        return $this->db->get('Customer')->result();
+    }
+    
+    public function store($request){
+        $this->name = $request->name;
+        $this->email = $request->email;
+        $this->token = $request->token;
+        $this->password = password_hash($request->password, PASSWORD_BCRYPT);
+        if($this->db->insert($this->table, $this)){
+            return ['msg' => 'Berhasil', 'error' => false];
+        }
+
+        return ['msg' => 'Gagal', 'error' => true];
+    }
+
+    public function login($request){
+        $this->email = $request->email;
+        $this->password = $request->password;
+        $query=$this->db->select(array('password', 'verified'))->where(array('email' => $this->email))->get($this->table)->row();
+        if(!empty($query)){
+            if(password_verify($this->password, $query->password) && $query->verified == 1){
+                $token = AUTHORIZATION::generateToken(['email' => $this->email]);
+                return ['msg' => "Berhasil", 'error' => false, 'token' => $token];
+            }
+        }
+
+        return ['msg' => 'Gagal', 'error' => true, 'token' => null];
+    }
+
+    public function emailVerification($request, $token){
+        $updateData = ['verified' => $request->verified];
+        if($this->db->where('token', $token)->update($this->table, $updateData)){
+            return ['msg' => 'Berhasil', 'error' => false];
+        }
+
+        return ['msg' => 'Gagal', 'error' => false];
+    }
+
+    public function update($request, $id){
+        $updateData = ['email' => $request->email, 'name' => $request->name];
+        if($this->db->where('id', $id)->update($this->table, $updateData)){
+            return ['msg' => 'Berhasil', 'error' => false];
+        }
+
+        return ['msg' => 'Gagal', 'error' => false];
+    }
+}
+
+?>
